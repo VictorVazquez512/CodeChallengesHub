8fcb88052f02b5ad50c29eac3216f34652543cd3
VictorVazquez512 <victorhugo.vazquezrojas@gmail.com>
2024-06-23 13:13:41 -0700

    2.MissingNumber Solved

--- USACOGuide/CSESIntroSet/2.MissingNumber.cpp
+++ USACOGuide/CSESIntroSet/2.MissingNumber.cpp
@@ -1,17 +1,73 @@
-"3.Repetitions.cpp",
-"4.IncreasingArray.cpp",
-"5.Permutations.cpp",
-"6.NumberSpiral.cpp",
-"7.TwoKnights.cpp",
-"8.TwoSets.cpp",
-"9.BitStrings.cpp",
-"10.TrailingZeros.cpp",
-"11.CoinPiles.cpp",
-"12.PalindromeReorder.cpp",
-"13.GrayCode.cpp",
-"14.TowerofHanoi.cpp",
-"15.CreatingStrings.cpp",
-"16.AppleDivision.cpp",
-"17.ChessboardandQueens.cpp",
-"18.DigitQueries.cpp",
-"19.GridPaths.cpp",
+/* 
+ID: samyok1
+TASK: ${Task_Name}
+LANG: C++11
+*/
+// Template.cpp last updated 6/7/18
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+// change these numbers
+const int MOD = 1000000007;
+const ll INF = 1e18;
+const int MX = 100001;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll n, t;
+    vl x;
+
+    cin >> n;
+
+    for (ll i = 0; i < n-1; ++i) {
+        cin >> t;
+        x.pb(t);
+    }
+
+    sort(all(x));
+    for (ll i = 1; i <= n; i++) {
+        if (x[i-1] != i){
+            cout << i;
+            return 0;
+        }
+    }
+
+    return 0;
+
+}
