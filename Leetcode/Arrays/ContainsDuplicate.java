8761b629e216983d2394eb89f6fe052e59fb53af
VictorVazquez512 <victorhugo.vazquezrojas@gmail.com>
2025-01-10 12:11:04 -0700

    217. Contains Duplicate (TS)

--- Leetcode/Arrays/ContainsDuplicate.java
+++ Leetcode/Arrays/ContainsDuplicate.java
@@ -30,18 +30,3 @@ class Solution {
 
     }
 }
-
-// Typescript version of Hash Set Approach
-function containsDuplicate(nums: number[]): boolean {
-    const numsSet = new Set<number>();
-
-    for (let i = 0; i < nums.length; i++) {
-        if (numsSet.has(nums[i])) {
-            return true;
-        }
-        numsSet.add(nums[i]);
-    }
-
-    return false;
-
-}
--- Leetcode/Arrays/ContainsDuplicate.ts
+++ Leetcode/Arrays/ContainsDuplicate.ts
@@ -1,1 +1,26 @@
-class ContainsDuplicate {}
+// Sorting Approach (less space complexity, more time complexity)
+function containsDuplicate(nums: number[]): boolean {
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == nums[i - 1]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// Hash Table Approach (more space complexity, less time complexity)
+function containsDuplicate(nums: number[]): boolean {
+  const numsSet: Set<number> = new Set<number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (numsSet.has(nums[i])) {
+      return true;
+    }
+    numsSet.add(nums[i]);
+  }
+
+  return false;
+}
