f02dca6323e3a6495dacda20ddeac7017d98c2ad
VictorVazquez512 <victorhugo.vazquezrojas@gmail.com>
2025-01-10 17:27:54 -0700

    Valid Anagram (Leetcode) (Java)

---
+++ Leetcode/Arrays/ValidAnagram.java
@@ -0,0 +1,29 @@
+// Sorting Approach
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        return Arrays.equals(sArray, tArray);
+
+    }
+}
+
+// Hashmap Approach
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) return false;
+            map.put(c, map.get(c) - 1);
+            if (map.get(c) == 0) map.remove(c);
+        }
+        return map.isEmpty();
+    }
+}
