43b83c908229a2888bbea1988b1ce7eba1bc5f77
VictorVazquez512 <victorhugo.vazquezrojas@gmail.com>
2025-01-10 20:44:14 -0700

    LongestPrefix (Leetcode) (Java)

---
+++ Leetcode/Arrays/LongestPrefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+
+        StringBuilder prefix = new StringBuilder();
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 0; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)) {
+                    return prefix.toString();
+                }
+            }
+            prefix.append(strs[0].charAt(i));
+        }
+
+        return prefix.toString();
+    }
+}
