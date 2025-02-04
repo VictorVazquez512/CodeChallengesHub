c99a15604fab76854ab5b4fe4770931a2167bf72
VictorVazquez512 <victorhugo.vazquezrojas@gmail.com>
2025-02-04 08:03:21 -0700

    Sort An Array (Leetcode) (Java)

---
+++ Leetcode/Arrays/SortAnArray.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    private int[] mergeSort(int[] arr, int l, int r){
+        if (l == r) {
+            return arr;
+        }
+
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+        return arr;
+    }
+
+    private int[] merge(int[] arr, int l, int m, int r) {
+        int[] left = Arrays.copyOfRange(arr, l, m + 1);
+        int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+
+        int i = l, j = 0, k = 0;
+
+        while (j < left.length && k < right.length) {
+            if (left[j] < right[k]) {
+                arr[i] = left[j];
+                j++;
+            } else {
+                arr[i] = right[k];
+                k++;
+            }
+
+            i++;
+        }
+
+        while (j < left.length) {
+            arr[i] = left[j];
+            j++;
+            i++;
+        }
+
+        while (k < right.length) {
+            arr[i] = right[k];
+            k++;
+            i++;
+        }
+
+        return arr;
+
+    }
+
+}
